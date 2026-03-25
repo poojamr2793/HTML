6d9d8a198986cf4b2c585909459c4402145d9889
pooja M R <poojamr2793@gmail.com>
2026-03-25 22:09:35 +0530

    Add files via upload

---
+++ Password generator/script.js
@@ -0,0 +1,37 @@
+function generatePassword() {
+  const length = document.getElementById("length").value;
+  const hasUpper = document.getElementById("uppercase").checked;
+  const hasLower = document.getElementById("lowercase").checked;
+  const hasNumber = document.getElementById("numbers").checked;
+  const hasSymbol = document.getElementById("symbols").checked;
+
+  const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const lower = "abcdefghijklmnopqrstuvwxyz";
+  const numbers = "0123456789";
+  const symbols = "!@#$%^&*()_+";
+
+  let chars = "";
+  if (hasUpper) chars += upper;
+  if (hasLower) chars += lower;
+  if (hasNumber) chars += numbers;
+  if (hasSymbol) chars += symbols;
+
+  if (chars === "") {
+    alert("Select at least one option!");
+    return;
+  }
+
+  let password = "";
+  for (let i = 0; i < length; i++) {
+    password += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+
+  document.getElementById("password").value = password;
+}
+
+function copyPassword() {
+  const passwordField = document.getElementById("password");
+  passwordField.select();
+  document.execCommand("copy");
+  alert("Password copied!");
+}
